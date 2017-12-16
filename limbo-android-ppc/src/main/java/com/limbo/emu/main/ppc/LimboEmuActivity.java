be34e00a83052692d6ac8b42d7713184502aef22
Limboemu <limboemu@limboemu.com>
2017-12-16 10:15:28 +0200

    move ppc emu files

---
+++ limbo-android-ppc/src/main/java/com/limbo/emu/main/ppc/LimboEmuActivity.java
@@ -0,0 +1,20 @@
+
+package com.limbo.emu.main.ppc;
+
+import com.max2idea.android.limbo.main.Config;
+import com.max2idea.android.limbo.main.LimboActivity;
+
+import android.os.Bundle;
+
+public class LimboEmuActivity extends LimboActivity {
+
+	public void onCreate(Bundle bundle){
+		Config.enable_PPC = true;
+		//Config.enableMTTCG = true;
+		Config.machinedir = Config.machinedir + "other/ppc_machines/";
+		
+		Config.osImages.put("Debian PowerPC Linux", "http://limboemulator.weebly.com/debian-powerpc-linux.html");
+		
+		super.onCreate(bundle);
+	}
+}
