7876a0de702e0227b67d3acad96f8b3a6ef56e69
Limboemu <limboemu@limboemu.com>
2017-12-16 20:23:14 +0200

    move pixman files

---
+++ limbo-android-lib/src/main/jni/pixman/config.h
@@ -0,0 +1,23 @@
+/* Name of package */
+#define PACKAGE "pixman"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "pixman@lists.freedesktop.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "pixman"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "pixman 0.30.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "pixman"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.30.0"
+
+/* The compiler supported TLS storage class */
+#define TLS __thread
