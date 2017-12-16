04be84c73a02a1b25939273f6db03ba9cce7bbc3
Limboemu <limboemu@limboemu.com>
2017-12-16 09:57:54 +0200

    move arm emu files

---
+++ limbo-android-arm/src/main/java/com/limbo/emu/main/arm/LimboEmuActivity.java
@@ -0,0 +1,19 @@
+package com.limbo.emu.main.arm;
+
+import com.max2idea.android.limbo.main.Config;
+import com.max2idea.android.limbo.main.LimboActivity;
+
+import android.os.Bundle;
+
+public class LimboEmuActivity extends LimboActivity {
+
+	public void onCreate(Bundle bundle){
+		Config.enable_ARM = true;
+		//Config.enableMTTCG = true;
+		Config.machinedir = Config.machinedir + "other/arm_machines/";
+		
+		Config.osImages.put("Debian ARM Linux", "http://limboemulator.weebly.com/debian-arm-linux.html");
+		
+		super.onCreate(bundle);
+	}
+}
