6c70908d652fadd3bd1a15025dc9a2a1b1f878fb
Limboemu <limboemu@limboemu.com>
2017-12-16 10:56:23 +0200

    new make/env files

---
+++ limbo-android-lib/src/main/jni/Android.mk
@@ -0,0 +1,24 @@
+#include $(call all-subdir-makefiles)
+
+include $(LIMBO_JNI_ROOT)/android-config.mak
+
+#If you want to build individual modules
+#include $(NDK_PROJECT_PATH)/jni/png/Android.mk
+#include $(NDK_PROJECT_PATH)/jni/jpeg/Android.mk
+include $(NDK_PROJECT_PATH)/jni/glib/Android.mk
+include $(NDK_PROJECT_PATH)/jni/compat/Android.mk
+#include $(NDK_PROJECT_PATH)/jni/openssl/Android.mk
+#include $(NDK_PROJECT_PATH)/jni/spice/Android.mk
+
+ifeq ($(USE_SDL),true)
+	include $(NDK_PROJECT_PATH)/jni/SDL/Android.mk
+	include $(NDK_PROJECT_PATH)/jni/SDL_image/Android.mk
+	include $(NDK_PROJECT_PATH)/jni/limbo/sdl_main/Android.mk
+endif
+
+ifeq ($(USE_SDL_AUDIO),true)
+	include $(NDK_PROJECT_PATH)/jni/SDL_mixer/Android.mk 
+endif
+
+include $(NDK_PROJECT_PATH)/jni/pixman/Android.mk
+include $(NDK_PROJECT_PATH)/jni/limbo/Android.mk
