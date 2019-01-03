b189c0e44a46881f175589177c2f13cb1abad7c3
limboemu <limboemu@example.com>
2019-01-04 01:20:55 +0200

    4.x standard dialog theme

--- limbo-android-lib/src/main/java/com/max2idea/android/limbo/utils/OSDialogBox.java
+++ limbo-android-lib/src/main/java/com/max2idea/android/limbo/utils/OSDialogBox.java
@@ -2,21 +2,16 @@ package com.max2idea.android.limbo.utils;
 
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.Iterator;
 
 import com.limbo.emu.lib.R;
 import com.max2idea.android.limbo.main.Config;
-import com.max2idea.android.limbo.main.LimboActivity;
-import com.max2idea.android.limbo.main.LimboFileManager;
-import com.max2idea.android.limbo.main.LimboSettingsManager;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -25,117 +20,100 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class OSDialogBox extends Dialog {
-	private static Activity activity;
-	private Button mOK;
+    public Spinner mOS;
+    public LinearLayout mLayout;
+    public String filetype;
+    private Button mGetDiskImageButton;
+    private Button mOk;
 
-	public OSDialogBox(Context context, int theme, Activity activity1) {
-		super(context, theme);
-		activity = activity1;
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND, WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-		setContentView(R.layout.oses_dialog);
-		this.setTitle("Select");
-		getWidgets();
-		setupListeners();
-		initUI();
+    public OSDialogBox(Context context) {
+        super(context);
 
-	}
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND, WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        setContentView(R.layout.oses_dialog);
+        this.setTitle("Select");
+        getWidgets();
+        setupListeners();
+        initUI();
 
-	@Override
-	public void onBackPressed() {
-		this.dismiss();
-	}
+    }
 
-	public static Spinner mOS;
-	public static LinearLayout mCDLayout;
-	
+    @Override
+    public void onBackPressed() {
+        this.dismiss();
+    }
 
-	public static boolean userPressedOS = true;
-	
+    private void getWidgets() {
+        mOS = (Spinner) findViewById(R.id.osimgval);
+        mLayout = (LinearLayout) findViewById(R.id.osimgl);
 
-	public static String filetype;
+        mGetDiskImageButton = (Button) findViewById(R.id.getDiskButton);
+        mGetDiskImageButton.setEnabled(true);
 
-	private void getWidgets() {
-		mOS = (Spinner) findViewById(R.id.osimgval);
-		mCDLayout = (LinearLayout) findViewById(R.id.osimgl);
+        mOk = (Button) findViewById(R.id.ok);
+    }
 
+    private void setupListeners() {
+        mGetDiskImageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String selectedOS = (String) mOS.getSelectedItem();
+                String url = null;
+                if (Config.osImages.containsKey(selectedOS))
+                    url = Config.osImages.get(selectedOS);
+                goToUrl(url);
 
-		mOK = (Button) findViewById(R.id.okButton);
-		mOK.setEnabled(false);
-	}
+            }
+        });
+        mOk.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        mOS.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-	private void setupListeners() {
-		mOK.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				String selectedOS = (String) mOS.getSelectedItem();
-				String url = null;
-				if(Config.osImages.containsKey(selectedOS))
-					url = Config.osImages.get(selectedOS);
-				browse(url);
-				dismiss();
-			}
-		});
-		mOS.setOnItemSelectedListener(new OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String cd = (String) ((ArrayAdapter<?>) mOS.getAdapter()).getItem(position);
+                if (position > 0)
+                    mGetDiskImageButton.setEnabled(true);
+                else
+                    mGetDiskImageButton.setEnabled(false);
+            }
 
-			public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-				String cd = (String) ((ArrayAdapter<?>) mOS.getAdapter()).getItem(position);
-				if(position >0)
-					mOK.setEnabled(true);
-				else
-					mOK.setEnabled(false);
-				
-				userPressedOS = true;
-			}
-
-			public void onNothingSelected(AdapterView<?> parentView) {
-				// your code here
-				// Log.v(TAG, "Nothing selected");
-			}
-		});
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+                // Log.v(TAG, "Nothing selected");
+            }
+        });
 
 
-	}
+    }
 
-	private void initUI() {
-		// Set spinners to values from currmachine
-		
-		populateCDRom("cd");
-
-	}
-
-	// Set CDROM
-	private static void populateCDRom(String fileType) {
-		userPressedOS = false;
-		
-		ArrayList<String> arraySpinner = new ArrayList<String>();
-		arraySpinner.add("None");
-		 Enumeration<String> iter = Config.osImages.keys();
-		while(iter.hasMoreElements()){
-			arraySpinner.add(iter.nextElement());
-		}
-		
-		ArrayAdapter<String> osAdapter = new ArrayAdapter<String>(activity, R.layout.custom_spinner_item, arraySpinner);
-		osAdapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
-		mOS.setAdapter(osAdapter);
-		mOS.invalidate();
-	}
+    private void initUI() {
+        populateSpinner();
+    }
 
 
-	
+    private void populateSpinner() {
+        ArrayList<String> arraySpinner = new ArrayList<>();
+        arraySpinner.add("Other");
+        Enumeration<String> iter = Config.osImages.keys();
+        while (iter.hasMoreElements()) {
+            arraySpinner.add(iter.nextElement());
+        }
 
+        ArrayAdapter<String> osAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, arraySpinner);
+        osAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mOS.setAdapter(osAdapter);
+        mOS.invalidate();
+    }
 
-	public static void browse(String url) {
-		
-		Intent i = new Intent(Intent.ACTION_VIEW);
-		i.setData(Uri.parse(url));
-		activity.startActivity(i);
+    public void goToUrl(String url) {
 
-	}
-
-	public static Intent getFileManIntent() {
-		return new Intent(activity, com.max2idea.android.limbo.main.LimboFileManager.class);
-	}
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        getContext().startActivity(i);
+    }
 
 }
