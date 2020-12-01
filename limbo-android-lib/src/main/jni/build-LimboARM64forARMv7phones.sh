5c0f28c43421cdfe975db59e96300dfa0d09edc7
limboemu <limboemu@example.com>
2020-12-01 20:43:21 +0200

    cleanup

--- limbo-android-lib/src/main/jni/build-LimboARM64forARMv7phones.sh
+++
@@ -1,7 +0,0 @@
-# for debugging
-#export USE_OPTIMIZATION=false
-#export NDK_DEBUG=1
-
-export BUILD_HOST=armeabi-v7a
-export BUILD_GUEST=aarch64-softmmu
-make limbo
--- limbo-android-lib/src/main/jni/build-Limbox86_64forx86phones.sh
+++
@@ -1,7 +0,0 @@
-# for debugging
-# export USE_OPTIMIZATION=false
-# export NDK_DEBUG=1
-
-export BUILD_HOST=x86
-export BUILD_GUEST=x86_64-softmmu
-make limbo
