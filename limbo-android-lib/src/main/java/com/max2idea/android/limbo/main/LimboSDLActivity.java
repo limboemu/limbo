b25413840598e09c1985330e60f06cda066acd51
vmobilis <75476228+vmobilis@users.noreply.github.com>
2021-07-30 15:33:48 +0300

    Use cutouts area in SDL mode

--- limbo-android-lib/src/main/java/com/max2idea/android/limbo/main/LimboSDLActivity.java
+++ limbo-android-lib/src/main/java/com/max2idea/android/limbo/main/LimboSDLActivity.java
@@ -900,7 +900,13 @@ public class LimboSDLActivity extends SDLActivity {
 
 		if (LimboSettingsManager.getFullscreen(this)) {
 			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-					WindowManager.LayoutParams.FLAG_FULLSCREEN);
+					WindowManager.LayoutParams.FLAG_FULLSCREEN); // 1024
+
+			if (VERSION.SDK_INT >= 28) {        // Android 9+ supports cutouts
+				getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;     // 1
+				if (VERSION.SDK_INT >= 30)  // Android 11
+					getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;  // 3
+			}
 
 			getWindow().getDecorView().setSystemUiVisibility(
 			   View.SYSTEM_UI_FLAG_LAYOUT_STABLE          // always use full screen
