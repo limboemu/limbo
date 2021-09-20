531ce45961f92e9f03f868111a285d7b8f499afd
limboemu <limboemu@example.com>
2021-09-20 19:06:03 +0300

    fix for settings

--- limbo-android-lib/src/main/java/com/max2idea/android/limbo/main/LimboSettingsManager.java
+++ limbo-android-lib/src/main/java/com/max2idea/android/limbo/main/LimboSettingsManager.java
@@ -26,6 +26,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceActivity;
